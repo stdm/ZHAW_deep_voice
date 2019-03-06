399036abafcc16c47a668b02b1e06c72801ed3d7
PatrickWalter214 <patrick.walter214@gmail.com>
2019-03-06 15:21:11 +0000

    Extended create container script while waiting for pickle generation

--- create_container.sh
+++ create_container.sh
@@ -1,7 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+DOCKER_USER="papommerman"
+DOCKER_REPO="benchmarking_speakerclustering"
+TAG="latest"
 read -p "Enter dockerhub username: " USERNAME
-read -p  "Enter dockerhub password: " PASSWORD
+read -s -p "Enter dockerhub password: " PASSWORD
+echo
 export SINGULARITY_DOCKER_USERNAME=$USERNAME
 export SINGULARITY_DOCKER_PASSWORD=$PASSWORD
-singularity pull docker://papommerman/benchmarking_speakerclustering:latest
-
+if [ -f ./$DOCKER_REPO-$TAG.simg ]; then
+	echo "File $DOCKER_REPO-$TAG already exists. Please delete it beforehand"
+else
+	singularity pull docker://$DOCKER_USER/$DOCKER_REPO:$TAG
+fi
