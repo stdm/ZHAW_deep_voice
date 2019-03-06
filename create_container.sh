d59f58f04c8778f68c7594241b5fb909a67beb6d
PatrickWalter214 <patrick.walter214@gmail.com>
2019-03-06 12:50:27 +0000

    Changed permissions for scripts so they can be executed

--- create_container.sh
+++ create_container.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 read -p "Enter dockerhub username: " USERNAME
-read -p "Enter dockerhub password: " PASSWORD
+read -p  "Enter dockerhub password: " PASSWORD
 export SINGULARITY_DOCKER_USERNAME=$USERNAME
 export SINGULARITY_DOCKER_PASSWORD=$PASSWORD
 singularity pull docker://papommerman/benchmarking_speakerclustering:latest
