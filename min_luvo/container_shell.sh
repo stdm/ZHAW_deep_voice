fd971421d643c289ace2c070b778e63f630411c6
PatrickWalter214 <patrick.walter214@gmail.com>
2019-03-20 17:38:06 +0000

    Adapted container_shell.sh file for min luvo

---
+++ min_luvo/container_shell.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+DEFAULT_NAME="benchmarking_speakerclustering-min_luvo"
+read -p "Enter name of the container: [$DEFAULT_NAME] " NAME
+NAME="${NAME:-$DEFAULT_NAME}"
+if [ -f ./$NAME.simg ]; then
+	 srun --pty --ntasks=1 --cpus-per-task=4 --mem=4G --qos=student-normal --gres=gpu:1 singularity shell $NAME.simg
+else
+	echo "File $NAME.simg does not exist."
+fi
