efdd1205c486e0c9a89694c44aca253a1d137b7e
neurudan <danielxiv64@gmail.com>
2019-03-21 18:46:17 +0100

    test

--- networks/pairwise_lstm/custom_iterator.py
+++ networks/pairwise_lstm/custom_iterator.py
@@ -14,7 +14,22 @@ class CustomIterator(mx.io.NDArrayIter):
 
     def getlabel(self):
         """Get label."""
+        print(self.label[0])
+        input('test')
         batch = self._batchify(self.label)
+        while 1:
+            for i in range((segments + bs - 1) // bs):
+                Xb = np.zeros((bs, 1, settings.FREQ_ELEMENTS, segment_size), dtype=np.float32)
+                yb = np.zeros(bs, dtype=np.int32)
+                # here one batch is generated
+                for j in range(0, bs):
+                    speaker_idx = randint(0, len(X) - 1)
+                    if y is not None:
+                        yb[j] = y[speaker_idx]
+                    spect = extract(X[speaker_idx, 0], segment_size)
+                    seg_idx = randint(0, spect.shape[1] - segment_size)
+                    Xb[j, 0] = spect[:, seg_idx:seg_idx + segment_size]
+                yield Xb, create_pairs(yb)
         print(batch.shape)
         input('test')
         return batch
