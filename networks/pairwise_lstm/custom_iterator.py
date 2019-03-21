5a0e0169bf8a3b2a6facc0cec2d28359a2ec0c98
neurudan <danielxiv64@gmail.com>
2019-03-21 17:50:08 +0100

    test

--- networks/pairwise_lstm/custom_iterator.py
+++ networks/pairwise_lstm/custom_iterator.py
@@ -1,12 +1,15 @@
 import mxnet as mx
+import numpy as np
 
 class CustomIterator(mx.io.NDArrayIter):
     @property
     def provide_label(self):
         """The name and shape of label provided by this iterator."""
         desc = []
+        speakers = np.amax(y) + 1
+
         for k, v in self.label:
-            print(tuple([self.batch_size] + list(v.shape[1:])))
+            print(tuple([self.batch_size] + list(v.shape[1:]) + [speakers]))
             input('test')
             desc.append(mx.io.DataDesc(k, tuple([self.batch_size] + list(v.shape[1:])), v.dtype))
         return desc
