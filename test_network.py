19bef937c5965b4a44e07ac9d08c19c37cce5017
neurudan <danielxiv64@gmail.com>
2019-03-22 14:00:11 +0100

    t

--- test_network.py
+++ test_network.py
@@ -2,16 +2,18 @@
 import mxnet as mx
 
 data = mx.sym.var('data')
-state1 = mx.sym.var('state1', init=mx.init.Normal(0.01))
-statecell1 = mx.sym.var('statecell1', init=mx.init.Normal(0.01))
-param1 = mx.sym.var('param1', init=mx.init.Normal(0.01))
-lstm1 = mx.sym.RNN(data=data, parameters=param1, state=state1, state_cell=statecell1, mode='lstm', state_size=256, num_layers=1)
-drop1 = mx.sym.Dropout(data=lstm1, p=0.5)
-state2 = mx.sym.var('state2', init=mx.init.Normal(0.01))
-statecell2 = mx.sym.var('statecell2', init=mx.init.Normal(0.01))
-param2 = mx.sym.var('param2', init=mx.init.Normal(0.01))
-lstm2 = mx.sym.RNN(data=drop1, parameters=param2, state=state2, state_cell=statecell2, mode='lstm', state_size=256, num_layers=1)
-dense1 = mx.sym.FullyConnected(data=lstm2, num_hidden=100 * 10)
+
+lstm_cell1 = mx.rnn.LSTMCell(num_hidden=256)
+begin_state1 = lstm_cell1.begin_state()
+lstm_output1, lstm_states1 = lstm_cell(data, begin_state1)
+
+drop1 = mx.sym.Dropout(data=lstm_output1, p=0.5)
+
+lstm_cell2 = mx.rnn.LSTMCell(num_hidden=256)
+begin_state2 = lstm_cell2.begin_state()
+lstm_output2, lstm_states2 = lstm_cell(data, begin_state2)
+
+dense1 = mx.sym.FullyConnected(data=lstm_output2, num_hidden=100 * 10)
 drop2 = mx.sym.Dropout(data=dense1, p=0.25)
 embedding = mx.sym.FullyConnected(data=drop2, num_hidden=100 * 5)
 m = 0.5
@@ -30,7 +32,7 @@ last_fc = last_fc-gt_one_hot
 out_list = [mx.symbol.BlockGrad(embedding)]
 softmax = mx.symbol.SoftmaxOutput(data=last_fc, label = gt_label, name='softmax', normalization='valid')
 out_list.append(softmax)
-if True:
+if False:
     #ce_loss = mx.symbol.softmax_cross_entropy(data=fc7, label = gt_label, name='ce_loss')/args.per_batch_size
     body = mx.symbol.SoftmaxActivation(data=last_fc)
     body = mx.symbol.log(body)
