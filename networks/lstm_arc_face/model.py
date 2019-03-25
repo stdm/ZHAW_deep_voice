65138379ca182c0dac4c4ea6d06ef354ea0475ad
neurudan <danielxiv64@gmail.com>
2019-03-25 15:48:09 +0100

    test

--- networks/lstm_arc_face/model.py
+++ networks/lstm_arc_face/model.py
@@ -71,7 +71,7 @@ class ArcFaceBlock(mx.gluon.HybridBlock):
             last_fc = last_fc + body
             last_fc = last_fc * self.s
 
-            ce_loss1 = F.softmax_cross_entropy(data=last_fc, label = label, name='ce_loss')
+            ce_loss1 = F.SoftmaxOutput(data=last_fc, label = label, name='softmax', normalization='valid')
 
             body = F.SoftmaxActivation(data=last_fc)
             body = F.log(body)
