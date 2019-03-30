6f769f912506adf2f04108ccb986d40ac64de10c
neurudan <danielxiv64@gmail.com>
2019-03-30 15:18:18 +0100

    first working version

--- networks/lstm_arc_face/settings.py
+++ networks/lstm_arc_face/settings.py
@@ -6,11 +6,23 @@ SENTENCES_PER_SPEAKER = 10
 SENTENCES_TRAIN = 8
 NUM_OF_SPEAKERS = 630
 
-# ArcFace Configs
+# ArcFace
+MARGIN_S = 1.0
+MARGIN_M1 = 1.0
+MARGIN_M2 = 0.3
+MARGIN_M3 = 0.2
+
+# Network Configs
+LSTM_HIDDEN_1 = 256
+LSTM_HIDDEN_2 = 256
+DENSE_HIDDEN_1 = 10
+DENSE_HIDDEN_2 = 5
+DROP_RATE_1 = 0.5
+DROP_RATE_2 = 0.25
 
 # Controller Configs
-MAX_EPOCHS = 10
+MAX_EPOCHS = 100
 BATCH_SIZE = 256
-BATCHES_PER_EPOCH = 10
+BATCHES_PER_EPOCH = 100
 TRAIN_DATA_NAME = 'speakers_100_50w_50m_not_reynolds_cluster'
 VAL_DATA_NAME = 'speakers_40_clustering_vs_reynolds'
