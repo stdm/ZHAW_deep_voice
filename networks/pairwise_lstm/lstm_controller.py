7b78bfd2ff4a0dca5f9753e389370304c6b7c382
PatrickWalter214 <patrick.walter214@gmail.com>
2019-04-12 16:15:42 +0200

    Fixed bug when loading data in lstm controller

--- networks/pairwise_lstm/lstm_controller.py
+++ networks/pairwise_lstm/lstm_controller.py
@@ -45,8 +45,8 @@ class LSTMController(NetworkController):
         logger.info('vec_size -> ' + str(vec_size))
 
         # Load and prepare train/test data
-        x_train, speakers_train, s_list_train = load_dev_test_data(self.get_validation_train_data(), self.val_data_size, 8)
-        x_test, speakers_test, s_list_test = load_dev_test_data(self.get_validation_test_data(), self.val_data_size, 2)
+        x_train, speakers_train, s_list_train = load_test_data(self.get_validation_train_data())
+        x_test, speakers_test, s_list_test = load_test_data(self.get_validation_test_data())
         x_train, speakers_train, = prepare_data(x_train, speakers_train, seg_size)
         x_test, speakers_test = prepare_data(x_test, speakers_test, seg_size)
 
