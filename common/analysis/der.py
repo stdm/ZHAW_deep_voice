9df5ce3324bb3386f8a0db6702928f284e38c261
PatrickWalter214 <patrick.walter214@gmail.com>
2019-04-16 14:23:02 +0200

    Prepared DER for integration into analysis

--- common/analysis/der.py
+++ common/analysis/der.py
@@ -1,24 +1,28 @@
 from pyannote.metrics.diarization import DiarizationErrorRate
 from pyannote.core import Segment, Annotation
 
-def diarization_error_rate(reference, hypothesis):
+def diarization_error_rate(y_true, y_pred, times):
     metric = DiarizationErrorRate()
+    reference = _generate_annotations(y_true, times)
+    hypothesis = _generate_annotations(y_pred, times)
     value = metric(reference, hypothesis)
     return value
+
+def _generate_annotations(utterances, times):
+    annotation = Annotation()
+    start = 0
+    for u, utterance in enumerate(utterances):
+        end = start + times[u]
+        annotation[Segment(start, end)] = utterance
+        start = end
+    return annotation
 
 
 if __name__ == '__main__':
     def flt_eq(x, y):
         return abs(x - y) < 1e-5
 
-    reference = Annotation()
-    reference[Segment(0, 8)] = 1
-    reference[Segment(8, 10)] = 1
-    reference[Segment(10, 18)] = 2
-    reference[Segment(18, 20)] = 2
-    hypothesis = Annotation()
-    hypothesis[Segment(0, 8)] = 1
-    hypothesis[Segment(8, 10)] = 2
-    hypothesis[Segment(10, 18)] = 2
-    hypothesis[Segment(18, 20)] = 2
-    assert flt_eq(diarization_error_rate(reference, hypothesis), 0.1)
+    y_true = [1, 1, 2, 2]
+    y_pred = [1, 2, 2, 2]
+    times = [8, 2, 8, 2]
+    assert flt_eq(diarization_error_rate(y_true, y_pred, times), 0.1)
