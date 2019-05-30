4453d24053c1f1fef1041486ba7aa056e5eec2ea
Jan Sonderegger <sondeja1@students.zhaw.ch>
2019-05-30 21:53:18 +0200

    testing implemented into gmm

--- networks/gmm/gmm_controller.py
+++ networks/gmm/gmm_controller.py
@@ -3,13 +3,13 @@ from common.network_controller import NetworkController
 from sklearn import mixture
 from common.utils.pickler import load, save
 from common.utils.paths import get_experiment_nets, get_speaker_pickle
+from common.clustering.generate_embeddings import generate_embeddings
 
 
 class GMMController(NetworkController):
     def __init__(self, config):
         super().__init__("gmm", config)
         self.network_file = self.name + "_100"
-
 
     def train_network(self):
         '''build a 16 component diagonal covariance GMM from the given features (usually 13 MFCCs)'''
@@ -26,15 +26,27 @@ class GMMController(NetworkController):
 
         save(model, get_experiment_nets(self.name))
 
-
     def get_embeddings(self):
-        X, y, speaker_names = load(self.get_validation_data_name())
+        X_train, y_train, speaker_train_names = load(self.get_validation_train_data())
+        X_test, y_test, speaker_test_names = load(self.get_validation_test_data())
+
         model = load(get_experiment_nets(self.name))
 
+        train_outputs = self.generate_outputs(X_train, model)
+        test_outputs = self.generate_outputs(X_test, model)
+
+        generate_embeddings(train_outputs, test_outputs, y_train, y_test, len(model))
+
+    def generate_outputs(self, X, model):
+        embeddings = []
         for sample in X:
+            sample = sample.reshape(sample.shape[1:]).transpose()
+            score_vector = []
+
             for speaker in model:  # find the most similar known speaker for the given test sample of a voice
                 score_per_featurevector = speaker['gmm'].score(sample)  # yields log-likelihoods per feature vector
+                score_vector.append(score_per_featurevector)
 
+            embeddings.append(score_vector)
 
-
-
+        return embeddings
