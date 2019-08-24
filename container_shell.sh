67a73b8eefbf265291429ee5ba2655bd48361227
PatrickWalter214 <patrick.walter214@gmail.com>
2019-08-24 12:58:13 +0200

    Updated script for gpu cluster

--- container_shell.sh
+++ container_shell.sh
@@ -3,7 +3,7 @@ DEFAULT_NAME="benchmarking_speakerclustering-latest"
 read -p "Enter name of the container: [$DEFAULT_NAME] " NAME
 NAME="${NAME:-$DEFAULT_NAME}"
 if [ -f ./$NAME.simg ]; then
-	 srun --pty --ntasks=1 --cpus-per-task=4 --mem=8G --qos=student-normal --gres=gpu:1 singularity shell $NAME.simg
+	 srun --pty --ntasks=1 --cpus-per-task=4 --mem=8G --qos=staff-normal --gres=gpu:1 singularity shell $NAME.simg
 else
 	echo "File $NAME.simg does not exist."
 fi
